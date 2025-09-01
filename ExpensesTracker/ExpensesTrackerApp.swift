6a6c35d6e9e264f0b6685db4737ad4d44b6faf00
savansamar <savansoni125@gmail.com>
2025-09-01 14:34:44 +0530

    Initial Commit

---
+++ ExpensesTracker/ExpensesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesTrackerApp.swift
+//  ExpensesTracker
+//
+//  Created by MACM72 on 01/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
